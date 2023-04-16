b5e41982bd55e913199a5395814fe9bba9c7d813
Dayoung Rho <76577003+dyrho1023@users.noreply.github.com>
2023-04-16 19:15:21 +0900

    MNIST File Upload

---
+++ MNIST/run.sh
@@ -0,0 +1,12 @@
+
+gcc MNIST.c -O3 -o MNIST.exe
+./MNIST.exe 
+#./lenet.out ../DB/MNIST/raw/image_0.raw
+
+#for((i=0;i<=10;i+=1))
+#do
+#	./lenet.out ../DB/MNIST/raw/image_$i.raw
+#done
+#	./lenet.out ../DB/MNIST/raw/image_30.raw
+
+	# 9998 and 9999
